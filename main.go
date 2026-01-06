0438f5587da01e6219d38b4c13c0814cf32fa7f9
msarar <mohammedsarar7@gmail.com>
2026-01-06 13:51:11 +0100

    groupie trcker

--- main.go
+++ main.go
@@ -7,15 +7,6 @@ import (
 	"html/template"
 )
 
-type Artist struct {
-	ID           int      `json:"id"`
-	Name         string   `json:"name"`
-	Image        string   `json:"image"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-}
-
 var artists []Artist
 
 func main() {
