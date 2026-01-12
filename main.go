3a70a14c680fc27ddf04672fbab994e18c5a524c
msarar <mohammedsarar7@gmail.com>
2026-01-12 17:33:36 +0100

    groupie trackar

--- main.go
+++ main.go
@@ -10,12 +10,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
-	// http.HandleFunc is a function that tells your Go web server: "When someone visits THIS url, run THAT function"
 	http.HandleFunc("/artist",handlers.ArtistHandler)
 	fmt.Println("Starting server on http://localhost:8080")
-	// opena a TCP socket start a infinite loop to listen for requests for each connection read the request process it and send back a response
-	// nil means use default server mux
-	// static
 	http.Handle("/static/",
         http.StripPrefix("/static/",
             http.FileServer(http.Dir("static"))))
